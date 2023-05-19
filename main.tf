5915a0e44665673b68e88e1b502713bb70ab7139
Matt Ralph <48395291+MattRalph@users.noreply.github.com>
2023-05-19 13:29:42 +1000

    Use module for security group

--- main.tf
+++ main.tf
@@ -24,11 +24,30 @@ resource "aws_instance" "blog" {
   instance_type = var.instance_type
 
   # tutorial 3.4  
-  vpc_security_group_ids = [aws_security_group.blog.id]
+  # vpc_security_group_ids = [aws_security_group.blog.id]
+
+  # tutorial 3.7  
+  vpc_security_group_ids = [module.blog_sg.security_group_id] # .security_group_id from https://registry.terraform.io/modules/terraform-aws-modules/security-group/aws/latest?tab=outputs
 
   tags = {
-    Name = "HelloWorld"
+    Name = "Learning Terraform"
   }
+}
+
+# tutorial 3.7
+module "blog_sg" {
+  source  = "terraform-aws-modules/security-group/aws"
+  version = "4.17.2"
+  name    = "blog_new"
+
+  vpc_id  = data.aws_vpc.default.id
+
+  ingress_rules       = ["http-80-tcp","https-443-tcp"]
+  ingress_cird_blocks = ["0.0.0.0/0"]
+
+  egress_rules       = ["all-all"]
+  egress_cird_blocks = ["0.0.0.0/0"]
+
 }
 
 # tutorial 3.4
