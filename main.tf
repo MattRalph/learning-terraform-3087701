d813b0c2978d7b350ddb4db7b5fbdf45fee1e9e3
Matt Ralph <48395291+MattRalph@users.noreply.github.com>
2023-05-19 13:58:44 +1000

    Creat dev vpc

--- main.tf
+++ main.tf
@@ -14,9 +14,29 @@ data "aws_ami" "app_ami" {
   owners = ["979382823631"] # Bitnami
 }
 
+/*
 # tutorial 3.4
 data "aws_vpc" "default" {
   default = true
+}
+*/
+
+# tutorial 4.1
+module "vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+
+  name = "dev"
+  cidr = "10.0.0.0/16"
+
+  azs             = ["us-west-2a", "us-west-2b", "us-west-2c"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+
+  enable_nat_gateway = true
+
+  tags = {
+    Terraform = "true"
+    Environment = "dev"
+  }
 }
 
 resource "aws_instance" "blog" {
@@ -40,7 +60,7 @@ module "blog_sg" {
   version = "4.17.2"
   name    = "blog" #"blog_new"
 
-  vpc_id  = data.aws_vpc.default.id
+  vpc_id  = module.vpc.public_subnets[0] # data.aws_vpc.default.id
 
   ingress_rules       = ["http-80-tcp","https-443-tcp"]
   ingress_cidr_blocks = ["0.0.0.0/0"]
