e5aa0b0a0935300bccf922e233bb8286fba0c7f0
Matt Ralph <48395291+MattRalph@users.noreply.github.com>
2023-05-19 14:27:24 +1000

    Create dev vpc

--- main.tf
+++ main.tf
@@ -34,8 +34,8 @@ module "blog_vpc" {
 resource "aws_instance" "blog" {
   ami           = data.aws_ami.app_ami.id
   instance_type = var.instance_type
-  subnet_id     = module.blog_vpc.public_subnets[0]
-  vpc_security_group_ids = [module.blog_sg.security_group_id] # .security_group_id from https://registry.terraform.io/modules/terraform-aws-modules/security-group/aws/latest?tab=outputs
+  
+  vpc_security_group_ids = [module.blog_sg.security_group_id] 
 
   tags = {
     Name = "Learning Terraform"
@@ -44,13 +44,13 @@ resource "aws_instance" "blog" {
 
 module "blog_sg" {
   source  = "terraform-aws-modules/security-group/aws"
-  version = "4.17.2" # "4.13.0"
+  version = "4.17.2" # "4.13.0" 
 
-  vpc_id  = module.blog_vpc.vpc_id
   name    = "blog"
+  vpc_id  = module.vpc.public_subnets[0]
+
   ingress_rules       = ["http-80-tcp","https-443-tcp"]
   ingress_cidr_blocks = ["0.0.0.0/0"]
   egress_rules       = ["all-all"]
   egress_cidr_blocks = ["0.0.0.0/0"]
-
 }
